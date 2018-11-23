b111336e36dfb3c05305d2c081970a32934f66f9
Valentin Dezat <valdezat@hotmail.fr>
2018-11-23 15:51:29 +0100

    lol

--- app/src/main/java/com/app/lesfousdeprog/motstordus/Accueil.java
+++ app/src/main/java/com/app/lesfousdeprog/motstordus/Accueil.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Accueil extends AppCompatActivity {
-
+private String yolesbg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
