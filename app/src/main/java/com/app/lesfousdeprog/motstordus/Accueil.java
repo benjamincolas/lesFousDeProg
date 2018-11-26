bc43ac296ad4cb1461dbc5e6cfeb48ab48425e38
benjamin colas <gociter@gmail.com>
2018-11-26 15:15:14 +0100

    première modif accueil

--- app/src/main/java/com/app/lesfousdeprog/motstordus/Accueil.java
+++ app/src/main/java/com/app/lesfousdeprog/motstordus/Accueil.java
@@ -1,12 +1,27 @@
 package com.app.lesfousdeprog.motstordus;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Accueil extends AppCompatActivity {
+public class Accueil extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn_quitter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        btn_quitter = (Button)this.findViewById(R.id.btn_quitter);
+        btn_quitter.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        finish();
     }
 }
