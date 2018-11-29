7c6b8eb5bcde6b3a67318d9ed4730fafd4eb5642
Benjamin Colas <benjamin.colas.pro@gmail.com>
2018-11-29 16:17:14 +0100

    ouais ouais

--- app/src/main/java/com/app/lesfousdeprog/motstordus/MemoRigolo.java
+++ app/src/main/java/com/app/lesfousdeprog/motstordus/MemoRigolo.java
@@ -1,17 +1,28 @@
 package com.app.lesfousdeprog.motstordus;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class MemoRigolo extends AppCompatActivity {
 
+    private ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memo_rigolo);
         setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-
+        image = (ImageView)this.findViewById(R.id.imageView);
+        image.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                image.setBackgroundResource(R.mipmap.logo_mediatheque);
+            }
+        });
     }
 }
