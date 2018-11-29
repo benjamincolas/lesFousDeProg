5f03ecbdd49c5051493e60fbd963176ed3950df6
Loïc Bouchain <LOIC41100@yahoo.fr>
2018-11-29 11:01:30 +0100

    Salut 1er sauvg

--- app/src/main/java/com/app/lesfousdeprog/motstordus/Accueil.java
+++ app/src/main/java/com/app/lesfousdeprog/motstordus/Accueil.java
@@ -1,35 +1,34 @@
 package com.app.lesfousdeprog.motstordus;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Range;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
-public class Accueil extends AppCompatActivity implements View.OnClickListener {
+public class Accueil extends AppCompatActivity {
 
     private Button btn_quitter;
     private Button btn_regles;
     private Button btn_jouer;
-
+private final int code_fenetre =20;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
         setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-
-        btn_quitter = (Button)this.findViewById(R.id.btn_quitter);
-        btn_quitter.setOnClickListener(this);
-
-        btn_regles = (Button)this.findViewById(R.id.btn_regles);
-        btn_regles.setOnClickListener(this);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         btn_jouer = (Button)this.findViewById(R.id.btn_jouer);
-        btn_jouer.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        finish();
+        btn_jouer.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            public void onClick(View view) {
+                Intent unIntent = new Intent(Accueil.this,RangeTaBd.class);
+                Accueil.this.startActivityForResult(unIntent, code_fenetre);
+            }
+        });
     }
 }
