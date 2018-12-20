b5298e354967d25398c7d7baacea3f0509fce5b0
Benjamin Colas <benjamin.colas.pro@gmail.com>
2018-12-20 14:17:54 +0100

    salut je push ça fait 10 ans

--- app/src/main/java/com/app/lesfousdeprog/motstordus/MemoRigolo.java
+++ app/src/main/java/com/app/lesfousdeprog/motstordus/MemoRigolo.java
@@ -1,173 +1,148 @@
 package com.app.lesfousdeprog.motstordus;
 
-import android.annotation.SuppressLint;
 import android.content.pm.ActivityInfo;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
 public class MemoRigolo extends AppCompatActivity {
 
     private int click = 0;
-    private ImageView[] imageView;
-    private ImageView imageClick;
+    private ArrayList<ImageView> imageViews;
     private ImageView choiceImage1;
     private ImageView choiceImage2;
     private Handler handler = new Handler();
+    private int difficulte;
+    private GridView gridImage;
+    private ArrayList<Integer> idImageView;
+    private int tempDifficulte;
+    private TextView memorigolo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memo_rigolo);
+
         setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        imageViews = new ArrayList<>();
+        idImageView = new ArrayList<>();
+        gridImage = this.findViewById(R.id.gridView);
+
+        difficulte = 12;
+        tempDifficulte = difficulte/2;
+
+        memorigolo = findViewById(R.id.memorigolo);
+
         setImage();
-
-        this.findViewById(R.id.imageView10).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                ImageView temp1 = imageView[0];
-                ImageView temp2 = findViewById(R.id.imageView10);
-                testClick(temp1, temp2);
-                testImage();
-            }
-        });
-
-        this.findViewById(R.id.imageView11).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                ImageView temp1 = imageView[1];
-                ImageView temp2 = findViewById(R.id.imageView11);
-                testClick(temp1, temp2);
-                testImage();
-            }
-        });
-
-        this.findViewById(R.id.imageView12).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                ImageView temp1 = imageView[2];
-                ImageView temp2 = findViewById(R.id.imageView12);
-                testClick(temp1, temp2);
-                testImage();
-            }
-        });
-
-        this.findViewById(R.id.imageView13).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                ImageView temp1 = imageView[3];
-                ImageView temp2 = findViewById(R.id.imageView13);
-                testClick(temp1, temp2);
-                testImage();
-            }
-        });
-
-        this.findViewById(R.id.imageView14).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                ImageView temp1 = imageView[4];
-                ImageView temp2 = findViewById(R.id.imageView14);
-                testClick(temp1, temp2);
-                testImage();
-            }
-        });
-
-        this.findViewById(R.id.imageView15).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                ImageView temp1 = imageView[5];
-                ImageView temp2 = findViewById(R.id.imageView15);
-                testClick(temp1, temp2);
-                testImage();
-            }
-        });
-
-        this.findViewById(R.id.imageView16).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                ImageView temp1 = imageView[6];
-                ImageView temp2 = findViewById(R.id.imageView16);
-                testClick(temp1, temp2);
-                testImage();
-            }
-        });
-
-        this.findViewById(R.id.imageView17).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                ImageView temp1 = imageView[7];
-                ImageView temp2 = findViewById(R.id.imageView17);
-                testClick(temp1, temp2);
-                testImage();
-            }
-        });
     }
 
+
     public void testImage(){
-        if(click == 2){
-            handler.postDelayed(new Runnable() {
-                public void run() {
-                    choiceImage1.setVisibility(View.VISIBLE);
-                    choiceImage2.setVisibility(View.VISIBLE);
-                    imageClick.setVisibility(View.INVISIBLE);
-                    click = 0;
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                if(choiceImage1.getTag() != choiceImage2.getTag()){
+                    choiceImage1.setVisibility(View.INVISIBLE);
+                    choiceImage2.setVisibility(View.INVISIBLE);
                 }
-            }, 1000);
-        }
+                click = 0;
+            }}, 700);
     }
 
     public void setImage(){
-        Integer[] listImage = new Integer[]{
-                R.mipmap.balai,
-                R.mipmap.batman,
-                R.mipmap.luffy,
-                R.mipmap.titeuf
-        };
-
-
-        imageView = new ImageView[]{
-                this.findViewById(R.id.imageView),
-                this.findViewById(R.id.imageView2),
-                this.findViewById(R.id.imageView3),
-                this.findViewById(R.id.imageView4),
-                this.findViewById(R.id.imageView5),
-                this.findViewById(R.id.imageView6),
-                this.findViewById(R.id.imageView7),
-                this.findViewById(R.id.imageView8)
-        };
-
         ArrayList<Integer> nbAlea = new ArrayList<>();
+        tempDifficulte-=1;
+        final Integer[] listImage = new Integer[]{
+                R.mipmap.batman,
+                R.mipmap.captain_america,
+                R.mipmap.captain_marvel,
+                R.mipmap.naruto,
+                R.mipmap.schtroumpf,
+                R.mipmap.son_goku,
+                R.mipmap.tintin,
+                R.mipmap.wonder_woman
+        };
 
+        if(difficulte == 8){
+            GridView dataGrid = findViewById(R.id.gridView);
+            dataGrid.setColumnWidth(450);
+            dataGrid.requestLayout();
+        }
+        else if(difficulte == 12){
+            GridView dataGrid = findViewById(R.id.gridView);
+            dataGrid.setColumnWidth(325);
+            dataGrid.requestLayout();
+        }
+        /*else if(difficulte == 16){
+            GridView dataGrid = findViewById(R.id.gridView);
+            dataGrid.setColumnWidth(250);
+            dataGrid.setNumColumns(5);
+            dataGrid.requestLayout();
+        }*/
 
-        nbAlea.add(0);
-        nbAlea.add(0);
-        nbAlea.add(1);
-        nbAlea.add(1);
-        nbAlea.add(2);
-        nbAlea.add(2);
-        nbAlea.add(3);
-        nbAlea.add(3);
-
-        for (ImageView imageTemp : imageView){
+        while(tempDifficulte > -1){
+            nbAlea.add(tempDifficulte);
+            nbAlea.add(tempDifficulte);
+            tempDifficulte --;
+        }
+        difficulte-=1;
+        while (difficulte > -1){
             int alea = (int) (nbAlea.size() * Math.random());
-            imageTemp.setBackgroundResource(listImage[nbAlea.get(alea)]);
-            imageTemp.setTag(listImage[nbAlea.get(alea)]);
+            ImageView iv = new ImageView(this);
+            iv.setImageResource(listImage[nbAlea.get(alea)]);
+            iv.setId(difficulte);
+            iv.setTag(listImage[nbAlea.get(alea)]);
+
+            imageViews.add(iv);
+            idImageView.add(listImage[nbAlea.get(alea)]);
+            difficulte --;
             nbAlea.remove(alea);
         }
+
+        GridAdapter adapter = new GridAdapter(MemoRigolo.this, idImageView);
+        gridImage.setAdapter(adapter);
+        gridImage.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MemoRigolo.this, "id image" + imageViews.get(position).getId(), Toast.LENGTH_SHORT).show();
+                view.setBackgroundResource(idImageView.get(position));
+                //testClick();
+            }
+        });
+
+        /*for (Integer uneImage : idImageView) {
+
+            this.findViewById(uneImage).setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    ImageView temp1 = imageViews.get(0);
+                    testClick(temp1);
+                }
+            });
+        }*/
     }
 
-    @SuppressLint("CutPasteId")
-    public void testClick(ImageView iv1, ImageView iv2){
+    public void testClick(ImageView iv1){
         click ++;
         if(click == 1){
-            imageClick = iv1;
-            imageClick.setVisibility(View.VISIBLE);
-            choiceImage1 = iv2;
-            choiceImage1.setVisibility(View.INVISIBLE);
+            choiceImage1 = iv1;
+            choiceImage1.setVisibility(View.VISIBLE);
         }
         else if(click == 2){
-            imageClick = iv1;
-            imageClick.setVisibility(View.VISIBLE);
-            choiceImage2 = iv2;
-            choiceImage2.setVisibility(View.INVISIBLE);
+            choiceImage2 = iv1;
+            choiceImage2.setVisibility(View.VISIBLE);
+
+            testImage();
         }
     }
 }
