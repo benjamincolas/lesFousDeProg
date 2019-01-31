79c3b1d94df7135bc9fa011a4baa569d6be1c077
Valentin Dezat <valdezat@hotmail.fr>
2019-01-31 16:12:09 +0200

    lol g pa dstag

--- app/src/main/java/com/app/lesfousdeprog/motstordus/Quizz.java
+++ app/src/main/java/com/app/lesfousdeprog/motstordus/Quizz.java
@@ -6,6 +6,7 @@ import android.content.pm.ActivityInfo;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
